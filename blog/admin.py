dba96ed966275e659cafd34c6cd167e9f077c207
Rohan Roy <b4you0870@gmail.com>
2015-03-27 16:52:38 +0530

    Post Admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,11 @@ from .models import Post
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
 	date_hierarchy = "created_at"
-	#fields = ("title","published")
+	fields = ('published',"title","content","author")
+	list_display = ["published","title","updated_at"]
+	list_display_links = ["title"]
+	list_editable = ["published"]
+	list_filter = ["published","updated_at","author"]
+
 	# pass
 admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
-from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True, editable=False)#save the timestamp when the model first creatred and not the field is editable in admin
@@ -9,7 +9,7 @@ class Post(models.Model):
 	slug = models.SlugField(max_length=255,blank=True,default='') #blank = True i.e it is not required for validatipn purpose , default = '' for not slug provided
 	content = models.TextField()
 	published = models.BooleanField(default=True)
-
+	author = models.ForeignKey(User, related_name="posts")
 	def __unicode__(self):
 		return self.title
 
