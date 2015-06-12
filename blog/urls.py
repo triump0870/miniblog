95f5535abe63b15497c8bbfe5007d7c7c943b02e
Rohan Roy <b4you0870@gmail.com>
2015-06-12 22:48:16 +0530

    Minified html

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, url
-from blog.views import PostListView, TagListView, PostDetailView, ProjectDetailView
+from blog.views import PostListView, TagListView, PostDetailView
 from . import feed
 urlpatterns = patterns('',
 	url(r"^$",PostListView.as_view(), name="list"),	
 	url(r"^tag/(?P<slug>[\w-]+)/$",TagListView.as_view(), name="tag"),
 	url(r"^(?P<slug>[\w-]+)/$",PostDetailView.as_view(), name="detail"),
-	url(r"^feed/$",feed.LatestPosts(), name='feed'),
-	
+	url(r"^feed/$",feed.LatestPosts(), name='feed')
 	)
