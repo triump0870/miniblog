f38b5ecb7e0c68097ee607f77d4dd2aba1cc60f2
Rohan Roy <b4you0870@gmail.com>
2016-12-27 14:07:01 +0530

    manage.py was updated

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miniblog.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ.get("DJANGO_SETTINGS_MODULE"))
 
     from django.core.management import execute_from_command_line
 
