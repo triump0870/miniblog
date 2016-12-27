4cac32f7aa6af31be1d88de37f699b1fa7a6c1e9
Rohan Roy <b4you0870@gmail.com>
2016-12-27 14:05:57 +0530

    development settings updated

---
+++ miniblog/settings/development.py
@@ -0,0 +1,86 @@
+from .base import *  # NOQA
+import sys
+import logging.config
+from os import makedirs
+
+STATIC_ROOT = join(dirname(BASE_DIR), 'static')
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATES[0]['OPTIONS'].update({'debug': True})
+
+# Turn off debug while imported by Celery with a workaround
+# See http://stackoverflow.com/a/4806384
+if "celery" in sys.argv[0]:
+    DEBUG = False
+
+# Django Debug Toolbar
+INSTALLED_APPS += (
+    'debug_toolbar.apps.DebugToolbarConfig',)
+
+# Show emails MIDDLEWARE_CLASSES
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+# to console in DEBUG mode
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Show thumbnail generation errors
+THUMBNAIL_DEBUG = True
+
+# Log everything to the logs directory at the top
+logfile_path = env('LOG_FILE_PATH')
+makedirs(logfile_path, exist_ok=True)
+LOGFILE_ROOT = join(BASE_DIR, logfile_path)
+
+# Reset logging
+# (see http://www.caktusgroup.com/blog/2015/01/27/Django-Logging-Configuration-logging_config-default-settings-logger/)
+
+LOGGING_CONFIG = None
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(pathname)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'django': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'maxBytes': 1024 * 1024 * 100,
+            'backupCount': 10,
+            'filename': join(LOGFILE_ROOT, 'django.log'),
+            'formatter': 'verbose'
+        },
+        'blog': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': join(LOGFILE_ROOT, 'blog.log'),
+            'formatter': 'verbose'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+    },
+    'loggers': {
+        'blog': {
+            'handlers': ['blog'],
+            'level': 'DEBUG',
+        },
+        'django.request': {
+            'handlers': ['django'],
+            'propagate': False,
+            'level': 'DEBUG',
+        },
+    }
+}
+
+logging.config.dictConfig(LOGGING)
