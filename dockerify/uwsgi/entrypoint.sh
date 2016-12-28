206f77eddb37d3cd93522e56082bfce11f77cdf7
Rohan Roy <b4you0870@gmail.com>
2016-12-28 19:17:34 +0530

    uwsgi docker was updated

--- dockerify/uwsgi/entrypoint.sh
+++ dockerify/uwsgi/entrypoint.sh
@@ -2,8 +2,19 @@
 
 set -e
 
-python manage.py makemigrations
-python manage.py migrate
+if [ "$DEBUG" == "True" ]; then
+	# check if the postgres service is up before starting django migration
+    until export MYSQL_PASSWORD=$MYSQL_PASSWORD; mysql -h "miniblog-mysql" -u "apps" -p"$MYSQL_PASSWORD" -e '\s; show databases'; do
+      >&2 echo "Mysql is unavailable - sleeping"
+      sleep 5
+    done
+
+    >&2 echo "Mysql is up - executing command"
+fi
+
+yes | python manage.py migrate
+yes | python manage.py collectstatic --noinput
+#python manage.py loaddata fixture miniblog.json
 
 # start uwsgi
 exec uwsgi --emperor dockerify/uwsgi/ --gid www-data
