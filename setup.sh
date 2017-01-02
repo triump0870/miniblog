708f2654522694b4b2800c5ec38ed36db721bbf3
Rohan Roy <b4you0870@gmail.com>
2017-01-02 18:52:08 +0530

    docker-compose was added

--- setup.sh
+++ setup.sh
@@ -14,4 +14,6 @@ apt-cache policy docker-engine
 sudo apt-get update
 sudo apt-get install -y docker-engine
 sudo apt-get autoremove -y
+curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 sudo service docker start
