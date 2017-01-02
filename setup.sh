7ff3dfcd924356cb3e5120ac115539e48ea04c47
Rohan Roy <b4you0870@gmail.com>
2017-01-02 19:02:46 +0530

    pip3 packages were added

--- setup.sh
+++ setup.sh
@@ -16,5 +16,6 @@ sudo apt-get install -y docker-engine
 sudo apt-get autoremove -y
 sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)"
 sudo chmod +x /usr/local/bin/docker-compose
-echo docker-compose -v
+sudo apt-get install -y pip3
+sudo pip3 install fabric3 django-environ
 sudo service docker start
