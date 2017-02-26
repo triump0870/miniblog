d68ad186c39801a2f1e692e8c6100c663272c37a
Rohan Roy <b4you0870@gmail.com>
2017-02-27 01:23:34 +0530

    env variable conf modified

--- dockerify/mysql/entrypoint.sh
+++ dockerify/mysql/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ -n "$DATABASE_USER" ] && [ -n "$DATABASE_PASSWORD" ] && [ -n "$DATABASE" ]; then
 	sed -e "s/{{DATABASE_USER}}/$DATABASE_USER/g" -e "s/{{DATABASE_PASSWORD}}/$DATABASE_PASSWORD/" -e "s/{{DATABASE}}/$DATABASE/g" createdb.conf.template > /docker-entrypoint-initdb.d/entrypoint.sh
 else
-	echo "ERROR - Must specify: -e DATABASE_USER and DATABASE_PASSWORD and DATABASE"
+	echo "ERROR - Must specify: -e DATABASE_USER, DATABASE_PASSWORD and DATABASE"
 	exit 1
 fi
 
