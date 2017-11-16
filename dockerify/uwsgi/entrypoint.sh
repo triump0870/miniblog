f8cc4cb31bf0791111dece5d6d10b6b55bfb8e08
Rohan Roy <b3you0870@gmail.com>
2017-11-17 04:48:56 +0530

    [Fixed] Django docker permission issue was fixed

--- dockerify/uwsgi/entrypoint.sh
+++ dockerify/uwsgi/entrypoint.sh
@@ -15,8 +15,8 @@ fi
 yes | python manage.py migrate
 
 # Forward app logs to docker log collector
-tail -n0 -F /var/logs/app_logs/*.log &
+tail -n0 -F /var/log/app_logs/*.log &
 
 # start uwsgi
-exec uwsgi --emperor dockerify/uwsgi/ --gid www-data
+exec uwsgi --emperor dockerify/uwsgi/ --gid app
 
