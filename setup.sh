ffbd6f3d0154a96e73c000cb29694df6de3ddf96
Rohan Roy <b3you0870@gmail.com>
2017-11-17 04:52:07 +0530

    Update the setup script

--- setup.sh
+++ setup.sh
@@ -17,13 +17,14 @@ apt-cache policy docker-engine
 sudo apt-get update
 sudo apt-get install -y docker-engine
 sudo apt-get autoremove -y
-sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)"
+sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.16.1/docker-compose-$(uname -s)-$(uname -m)"
 sudo chmod +x /usr/local/bin/docker-compose
 sudo apt-get install -y python3-pip
 export LC_ALL=C
 locale
 sudo pip3 install --upgrade pip
 sudo -H pip3 install fabric3 django-environ
+sudo chmod 666 /var/run/docker.sock
 sudo service docker start
 
 mv miniblog/settings/local.env.example miniblog/settings/local.env
