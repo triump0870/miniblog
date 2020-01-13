2be23b52e99ab78e860c74bbba6e336bbe5d078d
Rohan <b4you0870@gmail.com>
2020-01-13 22:26:54 +0530

    [Fix] Fixed Docker issue

--- setup.sh
+++ setup.sh
@@ -7,15 +7,14 @@ sudo apt-get -y upgrade
 sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 sudo apt-get install -y gcc mysql-client libmysqlclient-dev build-essential
 sudo apt-get install -y apt-transport-https ca-certificates
-sudo apt-key adv \
-           --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-           --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" \
- | sudo tee /etc/apt/sources.list.d/docker.list
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
 sudo apt-get update
-apt-cache policy docker-engine
+apt-cache policy docker-ce
 sudo apt-get update
-sudo apt-get install -y docker-engine
+sudo apt-get install -y docker-ce
 sudo apt-get autoremove -y
 sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.16.1/docker-compose-$(uname -s)-$(uname -m)"
 sudo chmod +x /usr/local/bin/docker-compose
