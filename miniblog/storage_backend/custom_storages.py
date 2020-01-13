c5ef2f39af80a1061bbabb8e5c920ff239b7273f
Rohan <b4you0870@gmail.com>
2020-01-13 20:24:46 +0530

    [Backend] Updated the storage backend based on the boto3

--- miniblog/storage_backend/custom_storages.py
+++ miniblog/storage_backend/custom_storages.py
@@ -3,36 +3,44 @@ from logging import getLogger
 from django.conf import settings
 from django.core.files.storage import get_storage_class
 from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 logger = getLogger("blog")
 
 
-class StaticStorage(S3BotoStorage):
-    """uploads to 'mybucket/static/', serves from 'cloudfront.net/static/'"""
-    location = settings.STATICFILES_LOCATION
+class StaticStorage(S3Boto3Storage):
+    location = 'static'
+    access_key = settings.AWS_S3_USER_KEY
+    secret_key = settings.AWS_S3_USER_SECRET
 
-    # access_key = settings.AWS_CLOUDFRONT_USER_KEY
-    # secret_key = settings.AWS_CLOUDFRONT_USER_SECRET
-
-    def __init__(self, *args, **kwargs):
-        kwargs['custom_domain'] = settings.AWS_CLOUDFRONT_DOMAIN
-        super(StaticStorage, self).__init__(*args, **kwargs)
-        # self.local_storage = get_storage_class('compressor.storage.CompressorFileStorage')
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    custom_domain = settings.AWS_CLOUDFRONT_DOMAIN
+    default_acl = 'public-read'
 
 
-class MediaStorage(S3BotoStorage):
-    """uploads to 'mybucket/media/', serves from 'cloudfront.net/media/'"""
-    location = settings.MEDIAFILES_LOCATION
-
-    # access_key = settings.AWS_CLOUDFRONT_USER_KEY
-    # secret_key = settings.AWS_CLOUDFRONT_USER_SECRET
-
-    def __init__(self, *args, **kwargs):
-        kwargs['custom_domain'] = settings.AWS_CLOUDFRONT_DOMAIN
-        super(MediaStorage, self).__init__(*args, **kwargs)
-        # self.local_storage = get_storage_class('compressor.storage.CompressorFileStorage')
+class PublicMediaStorage(S3Boto3Storage):
+    location = 'media'
+    access_key = settings.AWS_S3_USER_KEY
+    secret_key = settings.AWS_S3_USER_SECRET
+    file_overwrite = True
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    custom_domain = settings.AWS_CLOUDFRONT_DOMAIN
+    default_acl = 'public-read'
 
 
+# class MediaStorage(S3BotoStorage):
+#     """uploads to 'mybucket/media/', serves from 'cloudfront.net/media/'"""
+#     location = settings.MEDIAFILES_LOCATION
+#
+#     access_key = settings.AWS_CLOUDFRONT_USER_KEY
+#     secret_key = settings.AWS_CLOUDFRONT_USER_SECRET
+#
+#     def __init__(self, *args, **kwargs):
+#         kwargs['custom_domain'] = settings.AWS_CLOUDFRONT_DOMAIN
+#         super(MediaStorage, self).__init__(*args, **kwargs)
+#         # self.local_storage = get_storage_class('compressor.storage.CompressorFileStorage')
+#
+#
 class CacheS3BotoStorage(S3BotoStorage):
     """
     django-compressor uses this class to gzip the compressed files and send
